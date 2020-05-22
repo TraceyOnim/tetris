e5c2e305dee10b54afa3f081018e6fb2388c9e36
Tee22 <traceyOnim22@gmail.com>
2020-05-22 07:37:06 +0300

    test for new_random

--- tetris/test/brick_test.exs
+++ tetris/test/brick_test.exs
@@ -7,5 +7,13 @@ defmodule BrickTest do
     assert new_brick().name == :i
   end
 
+  test "creates a new random brick" do
+    actual = new_random()
+
+    assert actual.name in [:i, :l, :z, :o, :t]
+    assert actual.rotation in [0, 90, 180, 270]
+    assert actual.reflection in [true, false]
+  end
+
   def new_brick, do: new()
 end
